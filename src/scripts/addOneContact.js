275a4c7f1ace06db5d99c560d17a8a022092f24d
Word315 <boichuk315@gmail.com>
2025-07-06 16:54:43 +0200

    edit

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,21 @@
-export const addOneContact = async () => {};
+import { faker } from '@faker-js/faker';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+    try {
+        const newContact = faker.helpers.multiple(createFakeContact, {
+            count: 1,
+        });
+        const existingContacts = await readContacts();
+        existingContacts.push(...newContact);
+        await writeContacts(existingContacts);
+        return existingContacts;
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
 
 addOneContact();
--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+    try {
+        const contactsCount = await readContacts();
+        return `Number of contacts: ${contactsCount.length}`;
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
 
 console.log(await countContacts());
